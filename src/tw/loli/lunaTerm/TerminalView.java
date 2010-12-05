e908169c12f2ae49ee4a025808d9f5be5d433259
hrs <hrs@angela.loli.tw>
2010-12-05 17:03:47 +0800

    map "search_key" to "Ctrl" key in order to support those which didn't have a trackball.

--- src/tw/loli/lunaTerm/TerminalView.java
+++ src/tw/loli/lunaTerm/TerminalView.java
@@ -678,6 +678,7 @@ public class TerminalView extends View implements VDUDisplay {
 		case KeyEvent.KEYCODE_DPAD_RIGHT:
 			((vt320) buffer).keyPressed(vt320.KEY_RIGHT, ' ', metaState);
 			return true;
+		case KeyEvent.KEYCODE_SEARCH:
 		case KeyEvent.KEYCODE_DPAD_CENTER:
 			// TODO: Add some visual indication of Ctrl state
 			if (ctrlPressed) {
